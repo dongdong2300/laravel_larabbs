604d7483bd5aa01f8f0299f0800866e7c2eed33a
xuedong <dong_flying@163.com>
2020-05-27 09:31:25 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="float-right"><a href="mailto:dong_flying@163.com">联系我们</a></p>
+    </div>
+</footer>
