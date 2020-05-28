1e06f2e37f6133ae1bc47d8919a8aea2f7de43d0
xuedong <dong_flying@163.com>
2020-05-28 01:10:36 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,8 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
+// 资源路由
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');          // 显示页面
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');     // 编辑页面
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');    // 处理编辑
