7b3c052766b58dcdc23812c8827b5ef5c0df22bd
xuedong <dong_flying@163.com>
2020-07-05 14:38:39 +0000

    话题单元测试

--- app/Http/Resources/TopicResource.php
+++ app/Http/Resources/TopicResource.php
@@ -9,14 +9,19 @@ class TopicResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         $data = parent::toArray($request);
-        $data['user'] = new UserResource($this->whenLoaded('user'));
+
+        $data['user_id']     = (int)$data['user_id'];
+        $data['category_id'] = (int)$data['category_id'];
+
+        $data['user']     = new UserResource($this->whenLoaded('user'));
         $data['category'] = new CategoryResource($this->whenLoaded('category'));
+
         return $data;
     }
 }
