fdca9c0e0188b4ddd663a1058f99359e74052426
xuedong <dong_flying@163.com>
2020-07-05 08:55:14 +0000

    消息已读

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -12,4 +12,17 @@ class NotificationsController extends Controller
         $notifications = $request->user()->notifications()->paginate();
         return NotificationResource::collection($notifications);
     }
+
+    public function stats(Request $request)
+    {
+        return response()->json([
+            'unread_count' => $request->user()->notification_count,
+        ]);
+    }
+
+    public function read(Request $request)
+    {
+        $request->user()->markAsRead();
+        return response(null, 204);
+    }
 }
