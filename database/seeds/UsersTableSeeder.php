3715c24b244eaef3275e9a0b7c2b25f547580e84
xuedong <dong_flying@163.com>
2020-09-01 16:23:37 +0800

    管理员默认头像

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -40,7 +40,6 @@ class UsersTableSeeder extends Seeder
         $user         = User::find(1);
         $user->name   = 'xuedong';
         $user->email  = 'dong_flying@163.com';
-        $user->avatar = config('app.url') . '/uploads/images/avatars/202005/28/11_1590655003_zp2h6HLopc.png';
         $user->save();
 
         // 初始化用户角色，将 1 号用户指派为『站长』
